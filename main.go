98e6da4bf622c4c6709ac1e1d8c95decf45c6529
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-11-10 09:12:09 -0600

    Add RNG command group

--- main.go
+++ main.go
@@ -31,6 +31,33 @@ func main() {
 	router.On("ping", func(ctx *exrouter.Context) {
 		ctx.Reply("pong")
 	}).Desc("Responds with pong.")
+	// RNG command group
+	router.Group(func(r *exrouter.Route) {
+		r.Cat(randomCat)
+
+		r.On("coin", func(ctx *exrouter.Context) {
+			rand.Seed(time.Now().Unix())
+			coin = append(coin, "head", "tail")
+			message := fmt.Sprint(coin[rand.Intn(len(coin))])
+			ctx.Reply(message)
+		}).Desc("Flips a coin.").Cat(randomCat)
+
+		r.On("number", func(ctx *exrouter.Context) {
+			if length := len(ctx.Args); length == 1 {
+				ctx.Reply("Please provide a number for for the RNG.")
+			} else if govalidator.IsInt(ctx.Args[1]) {
+				input, err := strconv.Atoi(ctx.Args[1])
+				if err != nil {
+					message := fmt.Sprintf("Unable to convert %v to a number.", input)
+					ctx.Reply(message)
+				}
+				ctx.Reply(rand.Intn(input))
+			} else {
+				ctx.Reply(ctx.Args[1], " is not a number.")
+			}
+
+		}).Desc("Generates a random number.").Cat(randomCat)
+	})
 
 	router.Default = router.On("help", func(ctx *exrouter.Context) {
 		var text = ""
